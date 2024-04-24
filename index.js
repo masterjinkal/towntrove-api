0aaf6aad206503d9b694c40e519c0d2b374a7cf3
Jinkal Master <masterjinkal123@gmail.com>
2024-04-24 22:29:23 +0530

    Image URLS

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ app.use("/images", express.static("upload/images"));
 app.post("/upload", upload.single("product"), (req, res) => {
   res.json({
     success: 1,
-    mage_url: `https://${req.get("host")}/images/${req.file.filename}`,
+    image_url: `https://${req.get("host")}/images/${req.file.filename}`,
     //image_url: `${req.protocol}://${req.get("host")}/images/${req.file.filename}`,
   });
 });
