f229bdd50909e49c473e4f3d819b1be632766d42
Jinkal Master <masterjinkal123@gmail.com>
2024-04-24 19:45:08 +0530

    Modified

--- index.js
+++ index.js
@@ -12,17 +12,20 @@ const { response } = require("express");
 
 app.use(express.json());
 
+app.use(cors());
+
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
   );
   next();
 });
-
-// app.use(cors());
-
 //Database connection with MongoDB
 // mongoose.connect("mongodb+srv://masterjinkal:Jinkal123@cluster0.oiumjlq.mongodb.net/towntrove");
 mongoose.connect(
@@ -39,25 +42,23 @@ app.get("/", (req, res) => {
 //Image storage engine
 
 const storage = multer.diskStorage({
-    destination: "./upload/images",
-    filename: (req, file, cb) => {
-      return cb(
-        null,
-        `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`
-      );
-    },
-  });
-  
+  destination: "./upload/images",
+  filename: (req, file, cb) => {
+    return cb(
+      null,
+      `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`
+    );
+  },
+});
 
 const upload = multer({ storage: storage });
 
 // Creating upload end point for images
 app.use("/images", express.static("upload/images"));
-
 app.post("/upload", upload.single("product"), (req, res) => {
   res.json({
     success: 1,
-    image_url: `https://towntrove.onrender.com/images/${req.file.filename}`, 
+    image_url: `${req.protocol}://${req.get("host")}/images/${req.file.filename}`,
   });
 });
 
@@ -126,10 +127,8 @@ app.post("/addproduct", async (req, res) => {
     new_price: req.body.new_price,
     old_price: req.body.old_price,
   });
-  console.log(product);
   //now we have to save the product in the database
   await product.save();
-  console.log("Saved");
   //generate response for the frontend
   res.json({
     success: true,
@@ -140,7 +139,6 @@ app.post("/addproduct", async (req, res) => {
 //Creating API for deleting products (from our database)
 app.post("/removeproduct", async (req, res) => {
   await Product.findOneAndDelete({ id: req.body.id });
-  console.log("Removed");
   res.json({
     success: true,
     name: req.body.name,
@@ -149,7 +147,6 @@ app.post("/removeproduct", async (req, res) => {
 
 app.get("/allproducts", async (req, res) => {
   let products = await Product.find({});
-  console.log("All Products Fetched");
 
   // Modify products to include necessary details and image URL
   const modifiedProducts = products.map((product) => ({
@@ -265,11 +262,10 @@ app.get("/newcollection", async (req, res) => {
     new_price: product.new_price,
     old_price: product.old_price,
     // ... other desired details
-    image: product.image, // Construct image URL using retrieved filename
+    image: /images/${req.file.filename}, // Construct image URL using retrieved filename
   }));
 
   let newcollection = modifiedProducts.slice(1).slice(-8); //using this, we will get recently added new products in newcollection array
-  console.log("New Collection Fetched");
   res.send(newcollection);
 });
 
@@ -316,8 +312,6 @@ const fetchUser = async (req, res, next) => {
 //creating endpoint to add cartproducts in mongodb
 //creating endpoint for adding products in cartData
 app.post("/addtocart", fetchUser, async (req, res) => {
-  console.log(req.body, req.user);
-  console.log("added", req.body.itemId);
   //now using auth-token, we will find user and update cartdata in particular itemId
   //will have to create middleware
 
@@ -333,7 +327,6 @@ app.post("/addtocart", fetchUser, async (req, res) => {
 
 //creating endpoint/API to remove product frm cart data
 app.post("/removefromcart", fetchUser, async (req, res) => {
-  console.log("removed", req.body.itemId);
   let userData = await Users.findOne({ _id: req.user.id });
   //save that data in db
   if (userData.cartData[req.body.itemId] > 0) {
@@ -349,7 +342,6 @@ app.post("/removefromcart", fetchUser, async (req, res) => {
 //when we login an account, all the cartdata is retrieved
 //creating endpoint to get cart data
 app.post("/getcart", fetchUser, async (req, res) => {
-  console.log("GetCart");
   let userData = await Users.findOne({ _id: req.user.id });
   res.json(userData.cartData);
 });
