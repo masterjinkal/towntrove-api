816f93e24b1a65257f547b171f9696fe3e626493
Jinkal Master <masterjinkal123@gmail.com>
2024-04-24 19:25:24 +0530

    Upload

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ app.use("/images", express.static("upload/images"));
 app.post("/upload", upload.single("product"), (req, res) => {
   res.json({
     success: 1,
-    image_url: http//towntrove.onrender.com/images/${req.file.filename}, 
+    image_url: `https://towntrove.onrender.com/images/${req.file.filename}`, 
   });
 });
 
